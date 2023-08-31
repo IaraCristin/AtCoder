d12b6e9ee4873c4a7e8d848545bee6bf92af6d98
iara <iara@iara.com>
2023-08-31 19:38:56 -0300

    Frog1 complete

---
+++ Contests/Educational DP/Frog_1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+int main(){
+    //get N
+    int n;
+    cin >> n;
+
+    //get heights
+    vector<int> heights(n,0); //Initialize vector with all 0
+    for (int i = 0; i < n; i++)
+    {  
+        cin >> heights[i];   
+    }
+
+    vector<int> min_cost(n,0);
+    //The first stone has no cost because is where the frog starts
+    //And the second can only be accessed by the first
+    min_cost[1] = abs(heights[1]-heights[0]);
+
+    //Then we get the minimum cost to get to the i stone by adding the previous min cost of the i-1 or the i-2 stone to the cost of the i stone
+    for (int i = 2; i < n; i++)
+    {
+        min_cost[i] = min(  min_cost[i-1] + abs(heights[i] - heights[i-1]),     //Cost of jumping from i-1 stone to i stone
+                            min_cost[i-2] + abs(heights[i] - heights[i-2]));    //Cost of jumping from i-1 stone to i stone
+    }
+    
+    //And finally, we give the end result of the last cost
+    cout << min_cost[n-1];
+
+    //This is for the error code, it needs to return 0 to indicate that it ended correctly
+    return 0; 
+}
+
